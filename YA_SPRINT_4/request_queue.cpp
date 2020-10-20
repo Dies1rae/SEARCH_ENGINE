062cfd0645cf0e62e9f32a4c4f55bfebbbac5710
Poltavskiy Nick <56323635+Dies1rae@users.noreply.github.com>
2020-10-20 11:36:43 +0300

    Update request_queue.cpp

--- YA_SPRINT_4/request_queue.cpp
+++ YA_SPRINT_4/request_queue.cpp
@@ -1,11 +1,16 @@
 #include "request_queue.h"
-
-RequestQueue::RequestQueue(const SearchServer& search_server) :search_server_(search_server) {}
+RequestQueue::QueryResult::QueryResult():sec_(0){}
+RequestQueue::RequestQueue(const SearchServer& search_server) :search_server_(search_server) {
+    this->sec_now_ = 0;
+    this->zero_query = 0;
+    this->fill_query = 0;
+}
 
 std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
     if (this->sec_now_ < this->sec_in_day_) {
         this->sec_now_++;
-    } else {
+    }
+    else {
         this->sec_now_ = 0;
     }
     QueryResult query_res;
@@ -15,13 +20,19 @@ std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query,
     if (this->requests_.size() == this->sec_in_day_) {
         this->requests_.pop_back();
         this->requests_.push_front(query_res);
-    } else {
+    }
+    else {
         this->requests_.push_back(query_res);
     }
+    this->fill_query = count_if(this->requests_.begin(), this->requests_.end(), [](QueryResult& A) { return A.Searched_.size() > 0; });
     this->zero_query = std::count_if(this->requests_.begin(), this->requests_.end(), [](QueryResult& A) { return A.Searched_.size() == 0; });
     return query_res.Searched_;
 }
 
 int RequestQueue::GetNoResultRequests() const {
     return this->zero_query - 1;
+}
+
+int RequestQueue::GetResultRequests() const {
+    return this->fill_query + 1;
 }
