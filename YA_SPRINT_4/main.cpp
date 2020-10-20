01d78516fb086cae35ff85ebb4fb1e8cbaa4a436
Николай Полтавский <n.poltavskiy@5-tv.ru>
2020-10-20 12:08:43 +0300

    e boy

--- YA_SPRINT_4/main.cpp
+++ YA_SPRINT_4/main.cpp
@@ -9,7 +9,25 @@
 
 
 int main() {
-    TestSearchServer();
-    std::cout << "All test's are OK" << std::endl;
+    SearchServer search_server("и в на"s);
+    RequestQueue request_queue(search_server);
+
+    search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
+    search_server.AddDocument(2, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    search_server.AddDocument(3, "большой кот модный ошейник "s, DocumentStatus::ACTUAL, { 1, 2, 8 });
+    search_server.AddDocument(4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, { 1, 3, 2 });
+    search_server.AddDocument(5, "большой пёс скворец василий"s, DocumentStatus::ACTUAL, { 1, 1, 1 });
+
+    // 1439 запросов с нулевым результатом
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("пустой запрос"s);
+    }
+    // все еще 1439 запросов с нулевым результатом
+    request_queue.AddFindRequest("пушистый пёс"s);
+    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
+    request_queue.AddFindRequest("большой ошейник"s);
+    // первый запрос удален, 1437 запросов с нулевым результатом
+    request_queue.AddFindRequest("скворец"s);
+    cout << "Запросов, по которым ничего не нашлось "s << request_queue.GetNoResultRequests();
     return 0;
 }
